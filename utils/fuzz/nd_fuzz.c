5a1a4288bcbf3c0126b4804989f527c922d632f9
Joseph Tafese <jetafese@uwaterloo.ca>
2024-04-19 12:21:52 -0400

    feat: support fuzzer with harness

--- utils/fuzz/nd_fuzz.c
+++ utils/fuzz/nd_fuzz.c
@@ -21,43 +21,43 @@ jmp_buf g_jmp_buf;
     longjmp(g_jmp_buf, 1);                                             \
   }
 
-bool nd_bool(void) {
-  int tmp;
-  UPDATE_FUZZ_ITERATOR(bool)
-  memcpy(&tmp, g_fuzz_data_iterator, sizeof(bool));
-  g_fuzz_data_iterator += sizeof(bool);
-  return (tmp > 0);
-}
+// bool nd_bool(void) {
+//   int tmp;
+//   UPDATE_FUZZ_ITERATOR(bool)
+//   memcpy(&tmp, g_fuzz_data_iterator, sizeof(bool));
+//   g_fuzz_data_iterator += sizeof(bool);
+//   return (tmp > 0);
+// }
 
-int nd_int(void) {
-  int res;
+// int nd_int(void) {
+//   int res;
 
-  UPDATE_FUZZ_ITERATOR(int);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(int));
-  g_fuzz_data_iterator += sizeof(int);
+//   UPDATE_FUZZ_ITERATOR(int);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(int));
+//   g_fuzz_data_iterator += sizeof(int);
 
-  return res;
-}
+//   return res;
+// }
 
-uint8_t nd_uint8_t(void) {
-  uint8_t res;
+// uint8_t nd_uint8_t(void) {
+//   uint8_t res;
 
-  UPDATE_FUZZ_ITERATOR(uint8_t);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(uint8_t));
-  g_fuzz_data_iterator += sizeof(uint8_t);
+//   UPDATE_FUZZ_ITERATOR(uint8_t);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(uint8_t));
+//   g_fuzz_data_iterator += sizeof(uint8_t);
 
-  return res;
-}
+//   return res;
+// }
 
-uint16_t nd_uint16_t(void) {
-  uint16_t res;
+// uint16_t nd_uint16_t(void) {
+//   uint16_t res;
 
-  UPDATE_FUZZ_ITERATOR(uint16_t);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(uint16_t));
-  g_fuzz_data_iterator += sizeof(uint16_t);
+//   UPDATE_FUZZ_ITERATOR(uint16_t);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(uint16_t));
+//   g_fuzz_data_iterator += sizeof(uint16_t);
 
-  return res;
-}
+//   return res;
+// }
 
 uint32_t nd_bv32(void) {
   uint32_t res;
@@ -65,57 +65,62 @@ uint32_t nd_bv32(void) {
   UPDATE_FUZZ_ITERATOR(uint32_t);
   memcpy(&res, g_fuzz_data_iterator, sizeof(uint32_t));
   g_fuzz_data_iterator += sizeof(uint32_t);
-
-  return res;
-}
-
-uint64_t nd_uint64_t(void) {
-  uint64_t res;
-
-  UPDATE_FUZZ_ITERATOR(uint64_t);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(uint64_t));
-  g_fuzz_data_iterator += sizeof(uint64_t);
-
-  return res;
-}
-
-size_t nd_size_t(void) {
-  size_t res;
-
-  UPDATE_FUZZ_ITERATOR(size_t);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(size_t));
-  g_fuzz_data_iterator += sizeof(size_t);
-
-  return res;
-}
-
-void *nd_voidp(void) {
-  void *res;
-
-  UPDATE_FUZZ_ITERATOR(res);
-  memcpy(&res, g_fuzz_data_iterator, sizeof(res));
-  g_fuzz_data_iterator += sizeof(res);
-
-  return res;
-}
-
-void memhavoc(void *ptr, size_t size) {
-  if (!ptr) {
-    return;
-  }
-  if (g_fuzz_data_iterator + size - g_fuzz_data >= g_fuzz_data_size) {
-    longjmp(g_jmp_buf, 1);
-  }
-
-  memcpy(ptr, g_fuzz_data_iterator, size);
-  g_fuzz_data_iterator += size;
-}
-
-void __VERIFIER_assert(bool v) {
-  /* fprintf(stdout, "CHECK ASSERT\n"); */
+  fprintf(stdout, "[sea] nd_bv32: %d\n", res);
   fflush(stdout);
 
-  return assert(v);
+  return 1;
+}
+
+// uint64_t nd_uint64_t(void) {
+//   uint64_t res;
+
+//   UPDATE_FUZZ_ITERATOR(uint64_t);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(uint64_t));
+//   g_fuzz_data_iterator += sizeof(uint64_t);
+
+//   return res;
+// }
+
+// size_t nd_size_t(void) {
+//   size_t res;
+
+//   UPDATE_FUZZ_ITERATOR(size_t);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(size_t));
+//   g_fuzz_data_iterator += sizeof(size_t);
+
+//   return res;
+// }
+
+// void *nd_voidp(void) {
+//   void *res;
+
+//   UPDATE_FUZZ_ITERATOR(res);
+//   memcpy(&res, g_fuzz_data_iterator, sizeof(res));
+//   g_fuzz_data_iterator += sizeof(res);
+
+//   return res;
+// }
+
+// void memhavoc(void *ptr, size_t size) {
+//   if (!ptr) {
+//     return;
+//   }
+//   if (g_fuzz_data_iterator + size - g_fuzz_data >= g_fuzz_data_size) {
+//     longjmp(g_jmp_buf, 1);
+//   }
+
+//   memcpy(ptr, g_fuzz_data_iterator, size);
+//   g_fuzz_data_iterator += size;
+// }
+
+void __VERIFIER_assert(bool v, int property) {
+  fprintf(stdout, "CHECK ASSERT\n");
+  if (v) {
+    fprintf(stdout, "[sea] __VERIFIER_assert was called for property: %d\n", property);
+  }
+  fflush(stdout);
+
+  return;
 }
 
 void __SEA_assume(bool v) {
@@ -129,8 +134,8 @@ void __VERIFIER_assume(int v) {
   __SEA_assume(v);
 }
 
-void __VERIFIER_error(void) {
-  fprintf(stdout, "ERROR REACHED\n");
+void __VERIFIER_error() {
+  fprintf(stdout, "[sea] __VERIFIER_error was executed\n");
   fflush(stdout);
   assert(0);
 }
@@ -147,6 +152,11 @@ void btor2mlir_print_input_num(unsigned num, unsigned value, unsigned width) {
 
 void btor2mlir_print_state_num(unsigned num, unsigned value, unsigned width) { 
   fprintf(stdout, "state, %d, %d, %d\n", num, value, width);
+  fflush(stdout);
+}
+
+void btor2mlir_print_array_state_num(unsigned num, unsigned index, unsigned value, unsigned width) { 
+  fprintf(stdout, "state, %d, %d, %d, %d\n", num, index, value, width);
   fflush(stdout);
 }
 
@@ -167,15 +177,15 @@ int LLVMFuzzerTestOneInput(uint8_t *Data, size_t Size) {
   return 0;
 }
 
-bool nd_malloc_is_fail(void) { return nd_bool(); }
+// bool nd_malloc_is_fail(void) { return nd_bool(); }
 
-int nondet_compare(const void* a, const void *b) {
-  return nd_int();
-}
+// int nondet_compare(const void* a, const void *b) {
+//   return nd_int();
+// }
 
-/* nd_linked_list_node - special function for returning a symbolic value of
- * the linked list node pointer.
-*/
-struct aws_linked_list_node *nd_linked_list_node(void) {
-  return (struct aws_linked_list_node *)nd_voidp();
-}
+// /* nd_linked_list_node - special function for returning a symbolic value of
+//  * the linked list node pointer.
+// */
+// struct aws_linked_list_node *nd_linked_list_node(void) {
+//   return (struct aws_linked_list_node *)nd_voidp();
+// }
