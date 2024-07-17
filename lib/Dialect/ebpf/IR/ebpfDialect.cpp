aeb693a6bac48c0ed14ceefd760aa248c2e67c21
Joseph Tafese <jetafese@uwaterloo.ca>
2024-07-17 19:23:39 -0400

    feat: add inlining interface for ebpf

--- lib/Dialect/ebpf/IR/ebpfDialect.cpp
+++ lib/Dialect/ebpf/IR/ebpfDialect.cpp
@@ -7,6 +7,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "Dialect/ebpf/IR/ebpf.h"
+#include "mlir/Parser.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 
 #include "Dialect/ebpf/IR/ebpfOpsDialect.cpp.inc"
 
@@ -15,6 +18,43 @@
 
 using namespace mlir;
 using namespace mlir::ebpf;
+
+//===----------------------------------------------------------------------===//
+// ebpfDialect Interfaces
+//===----------------------------------------------------------------------===//
+namespace {
+/// This class defines the interface for handling inlining with ebpf ops
+struct ebpfInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+  ~ebpfInlinerInterface() override = default;
+
+  /// All call operations within ebpfDialect can be inlined.
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const final {
+    return true;
+  }
+
+  /// Returns true if the given region 'src' can be inlined into the region
+  /// 'dest' that is attached to an operation registered to the current dialect.
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       BlockAndValueMapping &) const final {
+    // Return true here when inlining into std func operation
+    auto *op = dest->getParentOp();
+    return isa<mlir::FuncOp>(op);
+  }
+
+  /// Returns true if the given operation 'op', that is registered to this
+  /// dialect, can be inlined into the region 'dest' that is attached to an
+  /// operation registered to the current dialect.
+  bool isLegalToInline(Operation *op, Region *dest, bool wouldBeCloned,
+                       BlockAndValueMapping &) const final {
+    return true;
+  }
+
+  /// ebpfDialect terminator operations don't really need any special handing.
+  void handleTerminator(Operation *op, Block *newDest) const final {}
+};
+} // namespace
 
 //===----------------------------------------------------------------------===//
 // ebpf dialect.
@@ -27,4 +67,5 @@ void ebpfDialect::initialize() {
 #define GET_OP_LIST
 #include "Dialect/ebpf/IR/ebpfOps.cpp.inc"
       >();
+  addInterfaces<ebpfInlinerInterface>();
 }
