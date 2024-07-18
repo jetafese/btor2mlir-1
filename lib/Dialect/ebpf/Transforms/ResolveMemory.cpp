978194f17d32d6112efb9a202d412451eea8b4ea
Joseph Tafese <jetafese@uwaterloo.ca>
2024-07-18 13:41:58 -0400

    chore: clang-format

--- lib/Dialect/ebpf/Transforms/ResolveMemory.cpp
+++ lib/Dialect/ebpf/Transforms/ResolveMemory.cpp
@@ -24,58 +24,62 @@ namespace {
 template <typename loadOp>
 LogicalResult replaceLoadWithLoadAddress(loadOp &op) {
   bool expectAddr = false, expectInt = false;
-  bool replaceWithMem = true, keepAsIs = true;
+  bool replaceWithMem = true, keepAsIs = false;
   for (auto &use : op.getResult().getUses()) {
     expectAddr = false, expectInt = false;
     if (isa<ebpf::StoreOp>(use.getOwner())) {
-        auto temp = cast<ebpf::StoreOp>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::StoreOp>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Store32Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Store32Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Store32Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Store16Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Store16Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Store16Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Store8Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Store8Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Store8Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::LoadOp>(use.getOwner())) {
-        auto temp = cast<ebpf::LoadOp>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::LoadOp>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Load32Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Load32Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Load32Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Load16Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Load16Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Load16Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else if (isa<ebpf::Load8Op>(use.getOwner())) {
-        auto temp = cast<ebpf::Load8Op>(use.getOwner());
-        expectAddr = (temp.lhs() == op.getResult());
-        expectInt = (temp.lhs() != op.getResult());
-        std::cerr << "************" << std::endl;
+      auto temp = cast<ebpf::Load8Op>(use.getOwner());
+      expectAddr = (temp.lhs() == op.getResult());
+      expectInt = (temp.lhs() != op.getResult());
+      std::cerr << "************" << std::endl;
     } else {
-        expectInt = true; expectAddr = false;
-        std::cerr << "xxxxxxxxxxxx" << std::endl;
+      expectInt = true;
+      expectAddr = false;
+      std::cerr << "xxxxxxxxxxxx" << std::endl;
     }
-    assert ((expectAddr && !expectInt) || (!expectAddr && expectInt));
-    replaceWithMem = expectAddr && replaceWithMem; keepAsIs = expectInt || keepAsIs;
-    assert ((replaceWithMem && !expectInt) || (!replaceWithMem && expectInt));
+    assert((expectAddr && !expectInt) || (!expectAddr && expectInt));
+    replaceWithMem = expectAddr && replaceWithMem;
+    keepAsIs = expectInt || keepAsIs;
+    assert((replaceWithMem && !expectInt) || (!replaceWithMem && expectInt));
   }
-  if (keepAsIs) { return failure(); }
+  if (keepAsIs) {
+    return failure();
+  }
   if (isa<ebpf::LoadOp>(op)) {
     ebpf::LoadOp temp = cast<ebpf::LoadOp>(op);
     auto opPtr = temp.getOperation();
@@ -95,6 +99,7 @@ struct ResolveMemoryPass : public ResolveMemoryBase<ResolveMemoryPass> {
   void runOnOperation() override {
     Operation *rootOp = getOperation();
     auto module_regions = rootOp->getRegions();
+    rootOp->dump();
     std::cerr << "there are " << module_regions.size() << " module regions"
               << std::endl;
     assert(module_regions.size() == 1 && "there isn't only one module region");
@@ -106,26 +111,26 @@ struct ResolveMemoryPass : public ResolveMemoryBase<ResolveMemoryPass> {
     std::cerr << "there are " << regions.getBlocks().size()
               << " blocks in first function" << std::endl;
     auto rescanAfterChanges = 1;
-    for (auto i = 0; i < rescanAfterChanges; ++i) {
-      for (auto &block : regions.getBlocks()) {
-        for (Operation &op : block.getOperations()) {
-          op.dump();
-          LogicalResult status =
-              llvm::TypeSwitch<Operation *, LogicalResult>(&op)
-                  // ebpf ops.
-                  .Case<ebpf::LoadOp>(
-                      [&](auto op) { return replaceLoadWithLoadAddress(op); })
-                  .Case<ebpf::Load32Op>(
-                      [&](auto op) { return replaceLoadWithLoadAddress(op); })
-                  .Case<ebpf::Load16Op>(
-                      [&](auto op) { return replaceLoadWithLoadAddress(op); })
-                  .Case<ebpf::Load8Op>(
-                      [&](auto op) { return replaceLoadWithLoadAddress(op); })
-                  .Default([&](Operation *) { return failure(); });
-          if (status.succeeded()) {
-            rescanAfterChanges++;
-          }
-        }
+    // for (auto i = 0; i < rescanAfterChanges; ++i) {
+    for (auto &block : regions.getBlocks()) {
+      for (Operation &op : block.getOperations()) {
+        op.dump();
+        LogicalResult status =
+            llvm::TypeSwitch<Operation *, LogicalResult>(&op)
+                // ebpf ops.
+                .Case<ebpf::LoadOp>(
+                    [&](auto op) { return replaceLoadWithLoadAddress(op); })
+                .Case<ebpf::Load32Op>(
+                    [&](auto op) { return replaceLoadWithLoadAddress(op); })
+                .Case<ebpf::Load16Op>(
+                    [&](auto op) { return replaceLoadWithLoadAddress(op); })
+                .Case<ebpf::Load8Op>(
+                    [&](auto op) { return replaceLoadWithLoadAddress(op); })
+                .Default([&](Operation *) { return failure(); });
+        // if (status.succeeded()) {
+        //   rescanAfterChanges++;
+        // }
+        // }
       }
     }
     std::cerr << "did something with a function" << std::endl;
